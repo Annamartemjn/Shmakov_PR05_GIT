dc696a6a26284a58f7a298287a0b63dde4288b31
Студент251 <student251@zamt.local>
2024-10-01 09:10:45 +0300

    Добавление фамилии на главную форму

--- pr05-git/AddForm.Designer.cs
+++ pr05-git/AddForm.Designer.cs
@@ -29,12 +29,34 @@ namespace pr05_git
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(240, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Шмаков Даниил Игоревич";
+            // 
+            // AddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "AddForm";
             this.Text = "AddForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
